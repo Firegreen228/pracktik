75c6098e078e25891a2957fa2a0a6b9bbf21208b
Firegreen228 <136463589+Firegreen228@users.noreply.github.com>
2023-06-13 16:35:38 +0400

    Add files via upload

---
+++ HelloWorldWebFlux/src/main/java/org/example/HelloWorldApplication.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
+
+@RestController
+class HelloWorldController {
+    @GetMapping("/")
+    public Mono<String> helloWorld() {
+        return Mono.just("Hello, World!");
+    }
+}
